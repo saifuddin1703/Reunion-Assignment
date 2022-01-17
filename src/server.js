d308654753f5fa478a2e5c2c611d58abc98701cf
saifuddin1703 <saifuddinahmed50@gmail.com>
2022-01-17 17:33:09 +0530

    error fixed

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const http = require('http');
 const app = require('./app');
-const dotenv = require('dotenv');
 const PORT = process.env.PORT || 5000;
-dotenv.config({
-    path : "src/.env"
-})
+
 const server = http.createServer(app);
 server.listen(PORT,()=>{
     console.log(`server is listening on port ${process.env.port}`);
+})
+
+const dotenv = require('dotenv');
+dotenv.config({
+    path : "src/.env"
 })
