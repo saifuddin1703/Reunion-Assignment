f2a880c49c13e0759808a5d8f17751a44e7299f9
saifuddin1703 <saifuddinahmed50@gmail.com>
2022-01-17 17:35:48 +0530

    changes

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 server.listen(PORT,()=>{
-    console.log(`server is listening on port ${process.env.port}`);
+    console.log(`server is listening on port ${PORT}`);
 })
 
 const dotenv = require('dotenv');
