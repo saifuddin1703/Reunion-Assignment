167be6a6291ab349ab33da1f6b0a59c12163fabe
saifuddin1703 <saifuddinahmed50@gmail.com>
2022-01-17 17:21:17 +0530

    errors resolved

--- src/server.js
+++ src/server.js
@@ -6,6 +6,6 @@ dotenv.config({
     path : "src/.env"
 })
 const server = http.createServer(app);
-server.listen(,()=>{
+server.listen(port,()=>{
     console.log(`server is listening on port ${process.env.port}`);
 })
